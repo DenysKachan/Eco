bc564422d987d0ea3812dcbaec3794db4cfe2cb3
Denys <denkacha220406@gmail.com>
2021-05-26 14:57:00 +0300

    Add preloader

---
+++ js/preloader.js
@@ -0,0 +1,11 @@
+document.body.onload = function () {
+    setTimeout(function () {
+        var preloader = document.getElementById('page-preloader');
+        if (preloader.classList.contains('done')) {
+            return;
+        } else {
+            preloader.classList.add('done');
+        }
+    }, 1000);
+
+};
